c34ddb666ea4927e4d2c3b7ba042087f09cfaa32
JJDeeds <jjdeeds57@gmail.com>
2016-04-27 16:09:06 -0500

    Magic carp do splash attack

---
+++ guitartunerproject/Debug/usbstk5505_gpio.pp
@@ -0,0 +1,9 @@
+# FIXED
+
+usbstk5505_gpio.obj: ../usbstk5505_gpio.c
+usbstk5505_gpio.obj: C:/Users/Deeds/workspace_v5_5/CCS_improved_loopback_eZdsp05/usbstk_bsl/usbstk5505_gpio.h
+usbstk5505_gpio.obj: C:/Users/Deeds/workspace_v5_5/CCS_improved_loopback_eZdsp05/usbstk_bsl/usbstk5505.h
+
+../usbstk5505_gpio.c: 
+C:/Users/Deeds/workspace_v5_5/CCS_improved_loopback_eZdsp05/usbstk_bsl/usbstk5505_gpio.h: 
+C:/Users/Deeds/workspace_v5_5/CCS_improved_loopback_eZdsp05/usbstk_bsl/usbstk5505.h: 
---
+++ guitartunerproject/Debug/usbstk5505_i2c.pp
@@ -0,0 +1,9 @@
+# FIXED
+
+usbstk5505_i2c.obj: ../usbstk5505_i2c.c
+usbstk5505_i2c.obj: C:/Users/Deeds/workspace_v5_5/CCS_improved_loopback_eZdsp05/usbstk_bsl/usbstk5505_i2c.h
+usbstk5505_i2c.obj: C:/Users/Deeds/workspace_v5_5/CCS_improved_loopback_eZdsp05/usbstk_bsl/usbstk5505.h
+
+../usbstk5505_i2c.c: 
+C:/Users/Deeds/workspace_v5_5/CCS_improved_loopback_eZdsp05/usbstk_bsl/usbstk5505_i2c.h: 
+C:/Users/Deeds/workspace_v5_5/CCS_improved_loopback_eZdsp05/usbstk_bsl/usbstk5505.h: 
